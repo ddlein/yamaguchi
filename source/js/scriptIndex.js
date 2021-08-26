b17a0e835df545349172019c3c6d7ce504ca68cc
Дима <70834132+ddlein@users.noreply.github.com>
2021-08-26 21:02:34 +0300

    Ð--fixup

--- source/js/scriptIndex.js
+++ source/js/scriptIndex.js
@@ -3,13 +3,13 @@ const down = document.querySelector('.main__button-down');
 
 
 up.addEventListener(('click'), () => {
-  gsap.to(".first-part", {
+  gsap.to(".main__first-part", {
     y: -30,
   });
 });
 
 down.addEventListener(('click'), () => {
-  gsap.to(".first-part", {
+  gsap.to(".main__first-part", {
     y: 20,
   });
 });
